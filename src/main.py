152772d1e4b4fa8e9b161822a5f89cc2af4a6cb5
Eric Day <eric.ch.day@gmail.com>
2018-10-18 12:21:37 -0600

    Now the calendar

--- src/main.py
+++ src/main.py
@@ -36,5 +36,30 @@ cspace.add_clients_from_array(cspace.clients_array)
 def client_page(name=None):
     return render_template('client_info.html', clients=cspace.clients, name=name)
 
+@app.route('/ws/<string:date>')
+def get_bookings_by_month(date):
+    monthly_booking = cspace.extract_bookings(cspace.workbook[date])
+    cspace.add_rooms_from_array(cspace.room_array, cspace.workbook['Rates'])
+    # monthly_booking = cspace.extract_bookings(date)
+    # print(monthly_booking)
+
+    colors = ['#C39BD3', '#7FB3D5', '#7DCEA0', '#F0B27A', '#808B96']
+    i = 0
+    dataRates = {}
+    for type, credit in cspace.rates.items():
+        dataRates[type] = {'Credits': credit, 'Color': colors[i]}
+        i = i + 1
+
+    cols = len(next(iter(monthly_booking.values() )))
+    rows = len(monthly_booking)
+    return render_template('base-calendar.html',
+                           monthly_booking=monthly_booking,
+                           rows=rows,
+                           cols=cols,
+                           rates=dataRates,
+                           facilities=cspace.rooms,
+                           date=date)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ src/templates/resources/js/calendar.js
@@ -0,0 +1,17 @@
+    function showTooltip(client) {
+          console.log('Yes');
+          var ev = window.event;
+          var posX = ev.clientX;
+          var posY = ev.clientY;
+          var element = document.elementFromPoint(posX, posY);
+          var elem = document.getElementById("tooltip");
+          elem.style.top=element.offsetTop+5+'px';
+          elem.style.left=posX+10+'px';
+          elem.style.visibility = "visible";
+          // elem.style.hover='pointer';
+          elem.innerHTML="<span class='tooltip-client'>"+client+"<BR>Room: "+room+"</span>";
+    }
+    function closeToolTip() {
+          var elem = document.getElementById("tooltip");
+          elem.style.visibility = "hidden";
+    }
