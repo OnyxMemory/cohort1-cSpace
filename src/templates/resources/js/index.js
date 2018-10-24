4d707d2c272d98c984a66e45976564b0f8167607
Ryker Rumsey <rykerrumsey@outlook.com>
2018-10-24 12:08:50 -0600

    fixed menu bar pill color on scroll

--- src/templates/resources/js/index.js
+++ src/templates/resources/js/index.js
@@ -1,20 +1,20 @@
 $(document).scroll(function() {
     $('nav').toggleClass('brighten', $(this).scrollTop() > 675)
     $('#bottom-nav').toggleClass('hidden', $(this).scrollTop() > 4350)
-    console.log($(this).scrollTop())
     
     if($(this).scrollTop() > 675) {
         $('.light-img').addClass('hidden')
         $('.dark-img').removeClass('hidden')
-        $('.nav-link').css('color', '#000')
         $('#menu-icon').css('color', '#000')
         $('#bottom-nav').addClass('bottom-nav--light')
+        $('#login').addClass('nav-link--black')
     } else {
         $('.light-img').removeClass('hidden')
         $('.dark-img').addClass('hidden')
-        $('.nav-link').css('color', '#fff')
+       // $('.nav-link').css('color', '#fff')
         $('#menu-icon').css('color', '#fff')
         $('#bottom-nav').removeClass('bottom-nav--light')
+        $('#login').removeClass('nav-link--black')
     }
 });
 
